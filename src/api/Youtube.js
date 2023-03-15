b5e006a1c5baae683f424a64d72dadfdb45bba15
GiorgiGudadze <gudadzegio317@gmail.com>
2023-03-15 07:16:34 +0400

    Trying to fix cors error

--- src/api/Youtube.js
+++ src/api/Youtube.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 const PEXEL_KEY = `563492ad6f917000010000010f378e522b0a4d7781692136b78222d1`;
 
 export default axios.create({
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': "GET, PUT, POST"
+    },
     baseURL:'https://api.pexels.com/videos',
     params:{
         Authorization:PEXEL_KEY
     }
+
 })
